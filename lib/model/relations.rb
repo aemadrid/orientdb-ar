1758732a1e0e3c85b9c6e31648088b3ddf0f084d
Adrian Madrid <aemadrid@gmail.com>
2011-01-19 15:03:38 -0700

    More relations

--- lib/model/relations.rb
+++ lib/model/relations.rb
@@ -1,12 +1,33 @@
 module OrientDB::AR
   module Relations
 
-    def has_one(klass, options = {})
+    def embedds_one(klass, options = {})
       klass = klass_for klass
       name = options[:name].to_s || field_name_for(klass, true)
 
-      field_type = klass.embedded? ? :embedded : :link
-      field name, [OrientDB::FIELD_TYPES[field_type], klass.oclass]
+      field name, [OrientDB::FIELD_TYPES[:embedded], klass.oclass]
+
+      class_eval <<-eorb, __FILE__, __LINE__ + 1
+        def #{name}                                                          # def address
+          doc = odocument[:#{name}]                                          #   doc = odocument[:address]
+          doc ? #{klass.name}.new_from_doc(doc) : nil                        #   doc ? Address.new_from_doc(doc) : nil
+        end                                                                  # end
+      eorb
+                                                                             #
+      class_eval <<-eorb, __FILE__, __LINE__ + 1
+        def #{name}=(value)                                                  # def address=(value)
+          raise "Invalid value for [#{name}]" unless value.is_a?(#{klass})   #   raise "Invalid value for [address]" unless value.is_a?(Address)
+          odocument[:#{name}] = value.odocument                              #   odocument[:address] = value.odocument
+          #{name}                                                            #   address
+        end                                                                  # end
+      eorb
+    end
+
+    def belongs_to(klass, options = {})
+      klass = klass_for klass
+      name = options.delete(:name) || field_name_for(klass, true)
+
+      field name, [OrientDB::FIELD_TYPES[:link], klass.oclass]
 
       class_eval <<-eorb, __FILE__, __LINE__ + 1
         def #{name}                                                          # def address
@@ -46,7 +67,7 @@ module OrientDB::AR
       eorb
 
       class_eval <<-eorb, __FILE__, __LINE__ + 1
-        def add_#{name.singularize}(value)                 # def add_address(value)
+        def add_#{name.to_s.singularize}(value)             # def add_address(value)
           odocument[:#{name}] << value.odocument            #   odocument[:addresses] << value.odocument
           #{name}                                           #   addresses
         end                                                 # end
